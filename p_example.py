e7b5c55995e957e5a04d00bca4347d354613d0a2
PrithakADHI <prithakad61@gmail.com>
2023-12-15 19:49:46 +0545

    Forreal Final Commit now lol

--- p_example.py
+++ p_example.py
@@ -1,28 +1,3 @@
-import PyGameKit as pg
-
-class MyGame(pg.Game):
-    def start(self):
-        pg.title("Tests")
-        self.catx = 0
-        self.caty = 0
-        self.cat = pg.load_image("cat.png")
-    def update(self): # Game Loop
-        pg.background((0, 0, 0))
-
-        mx, my = pg.mouse_getpos()
-        mb = pg.mouse_pressed()
-
-        if mb[0]:
-            self.catx, self.caty = mx - (200 / 2), my - (200 / 2)
-
-        
-        pg.display_image(self.cat, (self.catx, self.caty, 200, 200))
-
-        pg.update_screen()
-
-MyGame((800, 600), vsync=True, fps=60).run()
-
-"""
 # Same Program in PyGame:
 
 import pygame
@@ -52,4 +27,3 @@ while True:
     pygame.display.flip()
     screen.fill((0, 0, 0))
     clock.tick(60)
-"""
